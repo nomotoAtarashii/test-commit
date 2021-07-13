eefe092c3c16d18400cb4d0226eed9d6d8d052a2
nomotoAtarashii <tannmce150075@fpt.edu.vn>
2021-07-13 15:05:29 +0700

    tester

--- cypress/integration/test-goes-here/commit.test.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-describe('example test', () => {
-    
-    it('this is the test', () => {
-        cy.visit('http://imgexp.herokuapp.com', {setTimeout: 6000})
-    })
-
-})
